bb9c7f3d9da1319321fe2cd778e16ac1624fe8db
adityasaini0070 <adityasaini0070@gmail.com>
2026-03-26 12:14:21 +0000

    refactor: Externalize API paths to constants and add logging in AuthController.

--- src/main/java/com/aditya/resumebuilder/controller/AuthController.java
+++ src/main/java/com/aditya/resumebuilder/controller/AuthController.java
@@ -17,22 +17,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
+import static com.aditya.resumebuilder.util.AppConstants.*;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api/auth")
+@RequestMapping(AUTH_CONTROLLER)
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping("/register")
+    @PostMapping(REGISTER)
     public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
+        log.info("Inside AuthController - register(): {}", request);
         AuthResponse response = authService.register(request);
         log.info("response from register: {}", response);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping("/verifyemail")
+    @GetMapping(VERIFY_EMAIL)
     public ResponseEntity<?> verifyEmail(@RequestParam String token) {
+        log.info("Inside AuthController - verifyEmail(): {}", token);
         authService.verifyEmail(token);
         return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "email verified successfully"));
     }
--- src/main/java/com/aditya/resumebuilder/util/AppConstants.java
+++ src/main/java/com/aditya/resumebuilder/util/AppConstants.java
@@ -1,4 +1,7 @@
 package com.aditya.resumebuilder.util;
 
 public class AppConstants {
+    public static final String AUTH_CONTROLLER = "/api/auth";
+    public static final String REGISTER = "/register";
+    public static final String VERIFY_EMAIL = "/verifyemail";
 }
